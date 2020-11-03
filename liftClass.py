a99c82d9a218d1ec1c12b9da214a225b3a8286ff
bsstibich <bsstibich@gmail.com>
2020-11-03 03:44:43 -0600

    Progress on serialization
    -toJson() added to make the class serializable
    -yet to test whether or not I can deserialize

--- liftClass.py
+++ liftClass.py
@@ -1,25 +1,30 @@
 import json
+from json import JSONEncoder
 
 class Lift():
 	def __init__(self):
-		self._name = ""
+
+		self._name = "OHP"
 		self._tier = 2
 		self._sets = 5
 		self._reps = 3
 
 		_weight_progression = 5
 
-	def loadFile(self):
-		file = open('lifts.json', 'rb')
-		self = json.load(file)
-		file.close()
+	#def loadFile(self):
+		#file = open('lifts.json', 'r')
+		#self = json.load(file)
+		#file.close()
 
 	def saveFile(self):
-		file = open('lifts.json', 'wb')
-		json.dump(self, file)
+		file = open('lifts.json', 'w')
+		json.dump(self.to_json(), file)
 		file.close()
 
+	def to_json(self):
+		return json.dumps(self, default=lambda o: o.__dict__) 
+
+
 lift = Lift()
-#lift.saveFile()
-lift.loadFile()
-print(lift._name)
+lift.saveFile()
+
