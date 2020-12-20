1e980b19d187836c7d101c7c7b90ce3ef38828bc
bsstibich <bsstibich@gmail.com>
2020-12-19 22:23:49 -0600

    finish toolbar for now
    -toolbar.kv created and imported
    -still need to recolor and reallign the toolbar
    -would like to use left/right action items but can't get the screenmanger to work with it

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import kivy
 #from kivyMD.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivymd.theming import ThemeManager
@@ -12,6 +12,14 @@ Window.size = (360, 760) #s10+ scaled down
 
 
 class HomeTab(Screen):
+    def changeScreen(self):
+        sm = ScreenManager()
+        sm.add_widget(HomeTab(name='hometab'))
+        sm.add_widget(WorkoutTab(name='workouttab'))
+        sm.add_widget(SettingsTab(name='settingstab'))
+        sm.next()
+        
+        
     pass
 
 class WorkoutTab(Screen):
@@ -20,16 +28,15 @@ class WorkoutTab(Screen):
 class SettingsTab(Screen):
     pass
 
-#class WindowManager(ScreenManager):
-   # pass
 
 class Tracker(MDApp):
     def build(self):
         theme_cls = ThemeManager
         self.theme_cls.theme_style = "Dark"
-        sm = ScreenManager()
+        sm = ScreenManager(transition=NoTransition())
         sm.add_widget(HomeTab(name='hometab'))
         sm.add_widget(WorkoutTab(name='workouttab'))
+        sm.add_widget(SettingsTab(name='settingstab'))
         return sm
 
 if __name__ == "__main__":
