7347b541a0f6ca54e10af9722393dd7a040d821f
bsstibich <bsstibich@gmail.com>
2020-12-19 18:17:02 -0600

    start toolbar work

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import kivy
+#from kivyMD.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivymd.theming import ThemeManager
+
 
 kv = Builder.load_file("my.kv")
+Window.size = (360, 760) #s10+ scaled down
+
 
 class HomeTab(Screen):
     pass
@@ -13,12 +20,17 @@ class WorkoutTab(Screen):
 class SettingsTab(Screen):
     pass
 
-class WindowManager(ScreenManager):
-    pass
+#class WindowManager(ScreenManager):
+   # pass
 
-class Tracker(App):
-	def build(self):
-		return kv
+class Tracker(MDApp):
+    def build(self):
+        theme_cls = ThemeManager
+        self.theme_cls.theme_style = "Dark"
+        sm = ScreenManager()
+        sm.add_widget(HomeTab(name='hometab'))
+        sm.add_widget(WorkoutTab(name='workouttab'))
+        return sm
 
 if __name__ == "__main__":
 	Tracker().run()
